75214a49e679efe666aa52ef5e0cfc4bf8fbbbba
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2023-01-14 07:53:48 -0600

    feat: add about, contacts and rentals routes with hard-coded model

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,32 @@
+import { module, test } from 'qunit';
+import { visit, currentURL } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+
+module('Acceptance | list rentals', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically')
+  });
+
+  test('should list available rentals.', async function (assert) {
+    await visit('/');
+    const items = this.element.querySelectorAll('[data-testid="list-item"]');
+    assert.equal(items.length, 3, 'should display 3 items')
+  });
+
+  test('should link to information about the company.', async function (assert) {
+    
+  });
+
+  test('should link to contact information.', async function (assert) {
+  });
+
+
+  test('should filter the list of rentals by city.', async function (assert) {
+  });
+
+  test('should show details for a selected rental', async function (assert) {
+  });
+});
